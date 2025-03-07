dffbb744ddbe0f2b245b02456f0503badcdbf55f
Prajwol2058 <prajwol.bakhati@live.com>
2025-03-07 22:01:41 +0545

    feat:separate mock data to another file

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -54,30 +54,7 @@ import {
 } from "@/lib/validation/category-schema";
 import { toast } from "sonner";
 import { Category } from "@/types/category-types";
-
-// Mock data for categories
-const mockCategories = [
-  {
-    id: 1,
-    name: "Technology",
-    createdAt: "2023-01-15",
-    updatedAt: "2023-03-20",
-  },
-  { id: 2, name: "Health", createdAt: "2023-02-10", updatedAt: "2023-04-05" },
-  { id: 3, name: "Business", createdAt: "2023-03-22", updatedAt: "2023-03-22" },
-  {
-    id: 4,
-    name: "Education",
-    createdAt: "2023-04-18",
-    updatedAt: "2023-05-10",
-  },
-  {
-    id: 5,
-    name: "Entertainment",
-    createdAt: "2023-05-30",
-    updatedAt: "2023-06-15",
-  },
-];
+import { mockCategories } from "@/components/mockdata/mockdata";
 
 export default function CategoriesPage() {
   const [categories, setCategories] = useState<Category[]>([]);
---
+++ src/components/mockdata/mockdata.tsx
@@ -0,0 +1,22 @@
+export const mockCategories = [
+  {
+    id: 1,
+    name: "Technology",
+    createdAt: "2023-01-15",
+    updatedAt: "2023-03-20",
+  },
+  { id: 2, name: "Health", createdAt: "2023-02-10", updatedAt: "2023-04-05" },
+  { id: 3, name: "Business", createdAt: "2023-03-22", updatedAt: "2023-03-22" },
+  {
+    id: 4,
+    name: "Education",
+    createdAt: "2023-04-18",
+    updatedAt: "2023-05-10",
+  },
+  {
+    id: 5,
+    name: "Entertainment",
+    createdAt: "2023-05-30",
+    updatedAt: "2023-06-15",
+  },
+];
