141ea3df96b422e4c3bc4022082ab3afa0ff41ec
Prajwol2058 <prajwol.bakhati@live.com>
2025-03-09 21:50:03 +0545

    feat:implement logout functionality

--- src/components/dashboard/user-nav.tsx
+++ src/components/dashboard/user-nav.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { LogOut, User } from "lucide-react";
+import { useAuth } from "../hooks/use-auth";
 import { Button } from "../ui";
 
 export function UserNav() {
+  const { user, logout } = useAuth();
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-2">
         <User className="h-4 w-4" />
-        <span className="text-sm font-medium">{"Admin User"}</span>
+        <span className="text-sm font-medium">{user?.name}</span>
       </div>
-      <Button variant="ghost" size="icon">
+      <Button variant="ghost" size="icon" onClick={logout}>
         <LogOut className="h-4 w-4" />
         <span className="sr-only">Log out</span>
       </Button>
