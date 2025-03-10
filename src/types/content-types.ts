299112a49eb3078788a783f6f72089f5858f7cf3
Prajwol2058 <prajwol.bakhati@live.com>
2025-03-10 20:01:25 +0545

    feat: implement content searching and filtering

--- src/types/content-types.ts
+++ src/types/content-types.ts
@@ -1,20 +1,23 @@
 export interface Content {
-    id: number
-    slug: string
-    author_id: number
-    title: string
-    subtitle: string
-    content: string
-    category: string
-    createdAt: string
-  }
-  
-  export interface CreateContentData {
-    slug: string
-    author_id: number
-    title: string
-    subtitle: string
-    content: string
-    category: string
-  }
-  
+  id: number
+  slug: string
+  author_id: number
+  title: string
+  photo?: string
+  subtitle: string
+  content: string
+  size?: string
+  width?: number
+  height?: number
+  category: string
+  createdAt: string
+}
+
+export interface CreateContentData {
+  slug: string
+  author_id: number
+  title: string
+  subtitle: string
+  content: string
+  category: string
+}
